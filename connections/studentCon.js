e4069a21cfc79ffeccdd63bbb90d9b0c9cecf766
pgopasina <110560887+pgopasina@users.noreply.github.com>
2022-11-08 15:32:18 +0530

    Built Register and Login API's

---
+++ connections/studentCon.js
@@ -0,0 +1,20 @@
+var sequelize = require('sequelize');
+var myConnection = new sequelize('node_kt', 'root', 'root', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
+myConnection.authenticate()
+    .then(() => {
+        console.log('DB connected');
+    }).catch(() => {
+        console.log('DB not connected');
+    })
+
+myConnection.sync({ force: false })
+    .then((result) => {
+        console.log('Student table created', result);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+module.exports = myConnection;

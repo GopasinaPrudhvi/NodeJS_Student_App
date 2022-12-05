5ecec5df4ecd56706e2b45fc79ad9ddfbd39cd85
pgopasina <110560887+pgopasina@users.noreply.github.com>
2022-12-05 17:04:38 +0530

    worked on swagger

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -7,12 +7,24 @@ var verifyStdToken = require('../middleware/verifyStdToken')
 var expressJoi = require('express-joi-validation').createValidator({
     passError: true
 });
+var cors = require('cors')
+let options = {
+    origin:'*'
+}
+router.use(cors(options))
+
+// router.get('/stdToken', stdToken.stdToken)
+// router.post('/studentreg', verifyStdToken.verifyStdToken, registerSchema.stdValidating, studentService.studentReg);
+// router.post('/studentlog', verifyStdToken.verifyStdToken, studentService.studentLog);
+// router.get('/studentget', verifyStdToken.verifyStdToken, studentService.findStudents);
+// router.delete('/studentdel', verifyStdToken.verifyStdToken, studentService.removeStudent);
+// router.put('/studentup', verifyStdToken.verifyStdToken, studentService.updateStudent);
 
 router.get('/stdToken', stdToken.stdToken)
-router.post('/studentreg', verifyStdToken.verifyStdToken, registerSchema.stdValidating, studentService.studentReg);
-router.post('/studentlog', verifyStdToken.verifyStdToken, studentService.studentLog);
-router.get('/studentget', verifyStdToken.verifyStdToken, studentService.findStudents);
-router.delete('/studentdel', verifyStdToken.verifyStdToken, studentService.removeStudent);
-router.put('/studentup', verifyStdToken.verifyStdToken, studentService.updateStudent);
+router.post('/studentreg', registerSchema.stdValidating, studentService.studentReg);
+router.post('/studentlog', studentService.studentLog);
+router.get('/studentget', studentService.findStudents);
+router.delete('/studentdel/:id', studentService.removeStudent);
+router.put('/studentup', studentService.updateStudent);
 
 module.exports = router;
